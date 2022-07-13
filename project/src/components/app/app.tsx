897575e40178972a3a2e810a1fa3a26baac344e9
Елена <elenalecs@rambler.ru>
2022-07-14 00:06:16 +0400

    добавление компонентов страниц

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,19 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../../pages/main/main';
+
+type AppProps = {
+  data: {
+    image: string,
+    price: number,
+    name: string,
+    type: string,
+    bookMark: boolean,
+    isPremium: boolean,
+    rating: number
+  }[]
+}
+
+function App({data}:AppProps): JSX.Element {
+  return <Main list={data}/>;
 }
 
 export default App;
