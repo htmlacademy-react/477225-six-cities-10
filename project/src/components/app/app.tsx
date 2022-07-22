85db30f61683356f079f08d0440cb55507e65930
Елена <elenalecs@rambler.ru>
2022-07-22 23:02:17 +0400

    вынос роутера в отдельный компонент

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,27 +1,5 @@
-import {Card} from '../../types/types';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Main from '../../pages/main/main';
-import Login from '../../pages/login/login';
-import Favorites from '../../pages/favorites/favorites';
-import Room from '../../pages/room/room';
-import NotFoundPage from '../../pages/not-found-page/not-found-page';
-import PrivateRoute from '../private-route/private-route';
-import {AppRoute} from '../../const';
+import AppRouter from '../../router';
 
-type PropsType = {
-  cardList: Card[]
-}
-
-const App = ({cardList}: PropsType) => (
-  <BrowserRouter>
-    <Routes>
-      <Route path={AppRoute.Main} element={<Main cardList={cardList}/>}/>
-      <Route path={AppRoute.Login} element={<Login/>}/>
-      <Route path={AppRoute.Favorites} element={<PrivateRoute isAuth={false}><Favorites/></PrivateRoute>}/>
-      <Route path={AppRoute.Room} element={<Room/>}/>
-      <Route path={AppRoute.NotFoundPage} element={<NotFoundPage/>}/>
-    </Routes>
-  </BrowserRouter>
-);
+const App = () => <AppRouter />;
 
 export default App;
